b3330f6ece9897b7488091abdf814061829a0960
mraad@esri.com <mraad@esri.com>
2016-01-07 14:17:27 -0500

    Added PolygonType and PolygonUDT

--- src/main/scala/com/esri/udt/PointType.scala
+++ src/main/scala/com/esri/udt/PointType.scala
@@ -1,44 +1,41 @@
 package com.esri.udt
 
-import com.esotericsoftware.kryo.io.{Input, Output}
-import com.esotericsoftware.kryo.{Kryo, KryoSerializable}
 import com.esri.core.geometry.Point
 import org.apache.spark.sql.types.SQLUserDefinedType
 
 /**
   */
 @SQLUserDefinedType(udt = classOf[PointUDT])
-class PointType(var x: Double = 0.0, var y: Double = 0.0) extends SpatialType with KryoSerializable {
+class PointType(val x: Double = 0.0, val y: Double = 0.0) extends SpatialType {
 
   @transient lazy override val asGeometry = new Point(x, y)
 
-  def canEqual(other: Any): Boolean = other.isInstanceOf[PointType]
-
   override def equals(other: Any): Boolean = other match {
-    case that: PointType =>
-      (that canEqual this) &&
-        x == that.x &&
-        y == that.y
+    case that: PointType => x == that.x && y == that.y
     case _ => false
   }
 
   override def hashCode(): Int = {
-    val state = Seq(x, y)
-    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    Seq(x, y).foldLeft(0)((a, b) => {
+      val bits = java.lang.Double.doubleToLongBits(b)
+      31 * a + (bits ^ (bits >>> 32)).toInt
+    })
   }
 
   override def toString = s"PointType($x, $y)"
 
-  // TODO - Worth it ??, pass in precision
-  override def write(kryo: Kryo, output: Output): Unit = {
-    output.writeDouble(x)
-    output.writeDouble(y)
-  }
+  /*
+    // TODO - Worth it ??, pass in precision
+    override def write(kryo: Kryo, output: Output): Unit = {
+      output.writeDouble(x)
+      output.writeDouble(y)
+    }
 
-  override def read(kryo: Kryo, input: Input): Unit = {
-    x = input.readDouble()
-    y = input.readDouble()
-  }
+    override def read(kryo: Kryo, input: Input): Unit = {
+      x = input.readDouble()
+      y = input.readDouble()
+    }
+  */
 }
 
 object PointType {
--- src/test/scala/com/esri/app/Main.scala
+++ src/test/scala/com/esri/app/Main.scala
@@ -1,7 +1,7 @@
 package com.esri.app
 
+import com.esri.gdb._
 import com.esri.udt.PointType
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.{Logging, SparkConf, SparkContext}
 
 /**
@@ -22,20 +22,31 @@ object Main extends App with Logging {
 
   val sc = new SparkContext(conf)
   try {
-    val sqlContext = new SQLContext(sc)
-    val df = sqlContext.read.format("com.esri.gdb")
-      .option("path", path)
-      .option("name", name)
-      .option("numPartitions", "1")
-      .load()
-    df.printSchema()
-    df.registerTempTable(name)
-    sqlContext.udf.register("getX", (point: PointType) => point.x)
-    sqlContext.udf.register("getY", (point: PointType) => point.y)
-    sqlContext.udf.register("plus2", (point: PointType) => PointType(point.x + 2, point.y + 2))
-    // df.select("OBJECTID", "X", "Y", "Shape")
-    sqlContext.sql(s"select getX(plus2(Shape)),getX(Shape) as y from $name")
-      .show(20)
+    sc.gdbFile("/Users/mraad_admin/Share/World.gdb", "Cities", 1)
+      .map(row => {
+        row.getAs[PointType](row.fieldIndex("Shape")).asGeometry
+      })
+      .map(point => {
+        (point.getX, point.getY)
+      })
+      .foreach(println)
+
+    /*
+        val sqlContext = new SQLContext(sc)
+        val df = sqlContext.read.format("com.esri.gdb")
+          .option("path", path)
+          .option("name", name)
+          .option("numPartitions", "1")
+          .load()
+        df.printSchema()
+        df.registerTempTable(name)
+        sqlContext.udf.register("getX", (point: PointType) => point.x)
+        sqlContext.udf.register("getY", (point: PointType) => point.y)
+        sqlContext.udf.register("plus2", (point: PointType) => PointType(point.x + 2, point.y + 2))
+        // df.select("OBJECTID", "X", "Y", "Shape")
+        sqlContext.sql(s"select getX(plus2(Shape)),getX(Shape) as y from $name")
+          .show(20)
+    */
     /*
           .write
           .mode(SaveMode.Overwrite)
